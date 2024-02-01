bee8b46bbe193ffc987c95bcffdee8727dea95e0
TheJB4 <juanmer382@gmail.com>
2024-02-01 11:25:19 -0300

    agregamos estitca navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,8 +1,25 @@
+
+import { Navbar,Nav,Container,NavDropdown } from 'react-bootstrap';
+import Logo from '../../assets/Coffee_Logo.png'
+
 const Menu = () => {
-    return(
-        <div>
-            <h1>Menu</h1>
-        </div>
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={Logo} alt=""  className='img-fluid' width={150}/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#link">Administrador</Nav.Link>
+                        <Nav.Link href="#link">Registro</Nav.Link>
+                        <Nav.Link href="#link">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
