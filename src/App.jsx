0b523aae12ad0b2f8836b30caa92e31ca7c31d09
TheJB4 <juanmer382@gmail.com>
2024-02-01 11:06:30 -0300

    creamos los componentes y esqueleto de archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import Menu from './components/common/Menu'
+import Inicio from './components/pages/Inicio'
+import Footer from './components/common/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  
+      <Menu></Menu>
+      <Inicio></Inicio>
+      <Footer></Footer>
     </>
   )
 }
